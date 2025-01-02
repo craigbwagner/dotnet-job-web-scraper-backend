df9995a407118404b26876ac56be3d69afea2db6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-01-02 14:08:54 -0500

    delete docker and kubernetes related files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "Job Web Scraper";
+    config.Title = "Job Web Scraper v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
