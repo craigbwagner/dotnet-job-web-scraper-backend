f58f631a29735804c9f87fb17c38480aa9866a9d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-01-02 16:28:01 -0500

    scaffold ParsingService to handle HTML parsing

---
+++ Services/ParsingService.cs
@@ -0,0 +1,12 @@
+using dotnet_job_web_scraper_backend.Interfaces;
+using HtmlAgilityPack;
+
+namespace dotnet_job_web_scraper_backend.Services;
+
+public class ParsingService : IParsingService
+{
+    public void Fetch(string url)
+    {
+        return;
+    }
+}
